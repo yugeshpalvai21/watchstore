57040d4318dec84da4eb6addaf080e1d60ab5301
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-27 17:17:18 +0530

    implements charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,32 @@
 class ChargesController < ApplicationController
     def create
-        redirect_to root_path
+        @product = Product.find(params[:product_id])
+        @session = Stripe::Checkout::Session.create({
+            payment_method_types: ['card'],
+            line_items: [{
+              price_data: {
+                currency: 'inr',
+                product_data: {
+                  name: @product.title,
+                },
+                unit_amount: @product.price * 100,
+              },
+              quantity: 1,
+            }],
+            mode: 'payment',
+            # These placeholder URLs will be replaced in a following step.
+            success_url: success_charge_url(@product.id),
+            cancel_url: cancel_charge_url(@product.id)
+        })
+        respond_to do |format|
+           format.js 
+        end
+    end
+
+    def success
+        
+    end
+
+    def cancel
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :products
   root 'pages#home'
-  resources :charges, only: [:create]
+  resources :charges, only: [:create] do
+    member do
+      get 'success'
+      get 'cancel'
+    end
+  end
 end
