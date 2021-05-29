5516bd2cf7ad35bb29fa051c204aa00b2ddd882e
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-29 12:27:15 +0530

    adds filter to line_items create action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,12 +1,28 @@
 class LineItemsController < ApplicationController
     before_action :authenticate_user!
-
+    before_action :set_cart
+    before_action :set_product
 
     def create
-        current_user.cart.line_items.create(product_id: params[:product_id])
+        if item_already_added?
+            redirect_to request.referrer, notice: 'Product Already Added To Cart...'
+        else
+            LineItem.create(product: @product, cart: @cart) 
+            redirect_to request.referrer, notice: 'Product Added To Cart...'
+        end
+    end
+
+    private
+
+    def set_cart
         @cart = current_user.cart
-        @cart.grand_total += Product.find(params[:product_id]).price
-        @cart.save
-        redirect_to request.referrer
+    end
+
+    def set_product
+        @product = Product.find(params[:product_id])
+    end
+
+    def item_already_added?
+        @cart.line_items.pluck(:product_id).include? @product.id
     end
 end
