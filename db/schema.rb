45fc83dcb43c2c4d8ee0b4101a332a9619ff71ae
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-29 08:51:28 +0530

    generates migration to add image to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_114055) do
+ActiveRecord::Schema.define(version: 2021_05_29_031955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_114055) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "sales_count", default: 0, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
