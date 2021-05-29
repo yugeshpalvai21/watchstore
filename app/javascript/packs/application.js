f3b60116a149cda351f6f3feac8e26a6fa2eee60
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-29 08:09:43 +0530

    adds bootstrap using yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
 import "channels"
 
 Rails.start()
