5c2dbf1e0276bf2ebd78ca7695b2a24a477cc829
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-29 09:07:57 +0530

    bootstrapping products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :price)
+      params.require(:product).permit(:title, :price, :image)
     end
 end
