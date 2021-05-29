1b0e2f3fac09708f94ad50a0720ac944a95993cb
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-29 13:13:41 +0530

    adds cart details to navbar

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,12 @@
 class LineItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  after_create :update_cart_total
+
+  def update_cart_total
+   cart = self.cart
+   cart.grand_total += Product.find(product_id).price
+   cart.save 
+  end
 end
