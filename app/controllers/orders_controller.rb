4ceae1c4245185d57c9acc403a129f2f08ecea69
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-29 23:14:13 +0530

    refactoring orders index action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,6 @@ class OrdersController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        @orders = current_user.orders
+        @orders = current_user.orders.where(stripe_payment_status:  true)
     end
 end
