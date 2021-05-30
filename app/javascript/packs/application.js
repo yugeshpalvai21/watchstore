e4262d2729ed79eb272126995c82fdbb620e1d11
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-30 19:02:46 +0530

    adds bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "bootstrap/dist/js/bootstrap"
 import "channels"
 
 Rails.start()
